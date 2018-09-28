10ec1c1f25748272e24ebd66687ce5bf401adbac
yifanchen <yifanchen@synology.com>
2018-09-28 18:39:45 +0800

    Using project-base-dir-path on Cofigure. Such that able to execute from non-Main py-file.

--- ModelUtility/DataAccessor/Configure.py
+++ ModelUtility/DataAccessor/Configure.py
@@ -1,28 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
+import unittest
+
 from ModelUtility.DataAccessor.JsonAccessor.JsonAccessor import load_json, save_json
 
 
 class Configure(object):
-    def __init__(self):
+    def __init__(self, dir_path):
         super(Configure, self).__init__()
+        self.dir_path = dir_path
         self.default_config = None
         self.config = None
 
         self.load_default_config()
         self.load_config()
 
-    def load_config(self, path="./Data/ConfigUser.json"):
+    def load_config(self):
         try:
-            self.config = load_json(path)
+            self.config = load_json(self.dir_path + "ConfigUser.json")
         except FileNotFoundError:
             self.config = {}
 
-    def load_default_config(self, path="./Data/ConfigDefault.json"):
-        self.default_config = load_json(path)
+    def load_default_config(self):
+        self.default_config = load_json(self.dir_path + "ConfigDefault.json")
 
-    def save_config(self, path="./Data/ConfigUser.json"):
-        save_json(path, self.config)
+    def save_config(self):
+        save_json(self.dir_path + "ConfigUser.json", self.config)
 
     def __getitem__(self, name):
         try:
@@ -31,4 +35,45 @@ class Configure(object):
             return self.default_config[name]
 
 
-config = Configure()
+def get_matched_dir(path, project_dir_name="LifeRecorder"):
+    while True:
+        head, tail = os.path.split(path)
+        if tail == project_dir_name:
+            return path
+        elif head == path:
+            raise ValueError(path)
+        else:
+            path = head
+
+
+class _Test(unittest.TestCase):
+    EXPECT = "D:\Projects\LifeRecorder"
+
+    def assert_get_matched_dir(self, path, *args, **kwargs):
+        self.assertEqual(self.EXPECT, get_matched_dir(path, *args, **kwargs))
+
+    def test_get_matched_dir(self):
+        self.assert_get_matched_dir(self.EXPECT)
+        self.assert_get_matched_dir(self.EXPECT + "\ModelUtility\DataAccessor")
+
+    def test_get_matched_dir_invalid_path(self):
+        with self.assertRaises(ValueError):
+            self.assert_get_matched_dir("")
+
+        with self.assertRaises(ValueError):
+            self.assert_get_matched_dir(self.EXPECT[:-1])
+
+    def test_get_matched_dir_with_project_dir(self):
+        self.assert_get_matched_dir(self.EXPECT, "LifeRecorder")
+        self.assert_get_matched_dir(self.EXPECT + "\DDD\\", project_dir_name="LifeRecorder")
+
+        with self.assertRaises(ValueError):
+            self.assert_get_matched_dir(self.EXPECT, project_dir_name="AAA")
+
+
+config = {}
+if __name__ == "__main__":
+    unittest.main()
+else:
+    dir_path = get_matched_dir(os.getcwd()) + os.sep + "Data" + os.sep
+    config = Configure(dir_path)
