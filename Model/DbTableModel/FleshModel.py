88ebfcd2ff55a0132868d3fc4be8c30eab956a05
yifanchen <yifanchen@synology.com>
2018-11-09 18:40:16 +0800

    Show weekly count on flesh adding.

--- Model/DbTableModel/FleshModel.py
+++ Model/DbTableModel/FleshModel.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from collections import OrderedDict
+from datetime import timedelta
 
 from Model.DbTableModel.BaseModel import DurationModel, DurationType, Func
-from Model import Utility
+from Model import Utility, TimeUtility
 from Model.DataAccessor.DbTableAccessor import Timeline, Flesh, DoesNotExist, fn, JOIN
 
 
@@ -12,15 +13,19 @@ class FleshUtility(object):
     def add(date, count):
         if count <= 0:
             raise ValueError("Count should be greater than 0.")
+
         count_before = FleshUtility.get_count(date)
         count_after = count_before + count
-        if count_after > 2:
-            raise ValueError("Count will be %d (over 2)." % count_after)
+        if count_after > 3:
+            raise ValueError("Count will be %d (over 3) in one day." % count_after)
 
         date_id = Utility.get_or_create_date_id(date)
         Flesh.replace(date=date_id, count=count_after).execute()
 
-        return count_after
+        return {
+            'day': count_after,
+            'week': FleshUtility.get_week_count(date),
+            'last_week': FleshUtility.get_week_count(date - timedelta(days=7))}
 
     @staticmethod
     def get_count(date):
@@ -29,6 +34,17 @@ class FleshUtility(object):
             return timeline.flesh[0].count
         except (DoesNotExist, IndexError):
             return 0
+
+    @staticmethod
+    def get_week_count(date):
+        start, end = TimeUtility.get_week_start_and_end(date)
+        try:
+            record = FleshDurationModel.get_data(DurationType.WEEKLY).where(
+                start <= Timeline.date, Timeline.date <= end).get()
+        except DoesNotExist:
+            return 0
+        else:
+            return record.count
 
 
 class FleshDurationModel(DurationModel):
--- Model/TimeUtility.py
+++ Model/TimeUtility.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import datetime
+import unittest
 
 
 def covert_timedelta_to_time(timedelta):
@@ -13,3 +14,45 @@ def str_timedelta(timedelta):
 
 def covert_timedelta_to_hours_and_minutes(timedelta):
     return timedelta.seconds//3600, (timedelta.seconds//60) % 60
+
+
+def get_week_start_and_end(date):
+    start = date - datetime.timedelta(days=date.weekday())
+    end = date + datetime.timedelta(days=(6 - date.weekday()))
+    return start, end
+
+
+class GetWeekStartAndEndTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.monday = datetime.date(2018, 10, 15)
+        cls.days_in_one_week = tuple((cls.monday + datetime.timedelta(days=i)) for i in range(0, 7))
+        cls.day_in_next_week = datetime.date(2018, 10, 15) + datetime.timedelta(days=9)
+
+    def test_return_structure(self):
+        results = get_week_start_and_end(self.monday)
+        self.assertTrue(isinstance(results, tuple))
+        self.assertEqual(len(results), 2)
+        for result in results:
+            self.assertTrue(isinstance(result, datetime.date))
+
+    def test_day(self):
+        results = get_week_start_and_end(self.monday)
+        self.assertEqual(results[0], datetime.date(2018, 10, 15))
+        self.assertEqual(results[1], datetime.date(2018, 10, 21))
+
+        results = get_week_start_and_end(self.day_in_next_week)
+        self.assertEqual(results[0], datetime.date(2018, 10, 22))
+        self.assertEqual(results[1], datetime.date(2018, 10, 28))
+
+    def test_whole_week(self):
+        self.assertEqual(7, len(self.days_in_one_week))
+
+        for day in self.days_in_one_week:
+            start, end = get_week_start_and_end(day)
+            self.assertEqual(start, self.days_in_one_week[0])
+            self.assertEqual(end, self.days_in_one_week[6])
+
+
+if __name__ == '__main__':
+    unittest.main()
